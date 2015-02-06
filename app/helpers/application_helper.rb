3406b52b5fee59ae3d75c8541261bfead66af939
tranthuc <tran.chinh.thuc@framgia.com>
2015-02-06 15:04:42 +0700

    Add static pages and gems

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(title = '')
+		base_title = 'Training System'
+		if !title.empty?
+			"#{title} | #{base_title}"
+		else
+			base_title
+		end		
+	end
 end
