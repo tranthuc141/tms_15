834fb21ff4526cacf01d2c13590982ae18f2fe88
tranchinhthuc <thuctc91@gmail.com>
2015-03-05 16:11:19 +0700

    Admin for functions of course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -11,33 +11,7 @@ class CoursesController < ApplicationController
     @courses = Course.paginate page: params[:page]
   end
 
-  def new
-    @course = Course.new
-    @subjects = Subject.paginate page: params[:page]
-  end
-
-  def create
-    @course = Course.new course_params
-    if @course.save
-      flash[:success] = "Create course successfully!"
-      redirect_to courses_path
-    else
-      render 'new'
-    end   
-  end
-
-  def edit
-    @course = Course.find params[:id]
-  end
-
-  def update
-    @course = Course.find params[:id]
-    @course.update_attributes course_params
-    redirect_to course_path @course
-  end
-
   private
-
   def course_params
     params.require(:course).permit :name, :description,
       course_subject_relationships_attributes: [:id, :subject_id, :_destroy]
--- app/controllers/supervisor/subjects_controller.rb
+++ app/controllers/supervisor/subjects_controller.rb
@@ -1,2 +1,64 @@
 class Supervisor::SubjectsController < ApplicationController
+  before_action :supervisor
+
+  def index
+    @subjects = Subject.paginate page: params[:page]
+  end
+
+  def new
+    @subject = Subject.new
+    4.times{@task = @subject.tasks.build}
+  end
+
+  def create
+    @subject = Subject.new subject_params
+    if @subject.save
+      flash[:success] = "Create subject successfully!"
+      redirect_to supervisor_subjects_path
+    else
+      render 'new'
+    end 
+  end
+
+  def show
+    @subject = Subject.find params[:id]
+    @tasks = @subject.tasks
+    @task = Task.new
+  end
+
+  def edit
+    @subject = Subject.find params[:id]
+  end
+
+  def update
+    @subject = Subject.find params[:id]
+    @subject.update_attributes subject_params
+    redirect_to supervisor_subject_path @subject
+  end
+
+  def destroy
+    @subject = Subject.find params[:id]
+    @subject.destroy
+    flash[:success] = "Subject deleted"
+    redirect_to supervisor_subjetcs_path
+  end
+
+  private
+  def subject_params
+    params.require(:subject).permit :name, :description, :course_id,
+      :tasks_attributes => [:id, :description]
+  end
+
+  def supervisor
+    if logged_in?
+      unless current_user.is_supervisor?
+        flash[:danger] = "You must be supervisor"
+        redirect_to root_url
+      end
+    else
+      store_location
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end    
+  end
 end
